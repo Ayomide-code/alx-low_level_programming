760507db97fbf5f5a222356488ad104023ef67c7
ayomide-code <sholajaayomide10@gmail.com>
2022-09-11 14:40:12 -0700

    task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Prints 3 combination of numbers
+ * Return:Always (Success)
+ */
+int main(void)
+{
+	int c, i, k, j;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (((k + j) > (c + i) && k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
