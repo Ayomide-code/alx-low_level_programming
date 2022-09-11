cb1529680a5e3e7023a756602560c27504b9878b
ayomide-code <sholajaayomide10@gmail.com>
2022-09-11 06:26:51 -0700

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else if (lastdigit < 6 && lastdigit != 0)
 	{
-		printf("Last digit of %d is %d is less than 6 and not 0\n", n, lastdigit);
+		printf("Last digit of %dis %d and is less than 6 and not 0\n", n, lastdigit);
 	}
 	return (0);
 }
