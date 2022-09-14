26c1718f09bf16fb8ce01b1bfb92eb9749f93eb8
ayomide-code <sholajaayomide10@gmail.com>
2022-09-14 07:42:04 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for all aphabet character
+ * @c: the int  argument that will be used
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
+			{
+			return (1);
+			}
+			else
+			return (0);
+			}
+}
+
