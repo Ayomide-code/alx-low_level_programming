77673a73eb2bf661cafe2de1a6636d8dd7a2d848
ayomide-code <sholajaayomide10@gmail.com>
2022-09-14 16:32:13 -0700

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,11 +9,11 @@ int _isalpha(int c)
 
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 
-			{
-			return (1);
-			}
-			else
+	{
+		return (1);
+	}
 
-				return (0);
+	else
+		return (0);
 }
 
