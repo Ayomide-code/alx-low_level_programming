f889a4baf20bfe94cb4e1da2a73e0d8a879f79bf
ayomide-code <sholajaayomide10@gmail.com>
2022-09-14 06:12:03 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function to ptint abc
+ * Retun: 0
+ */
+void print_alphabeth(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
