4444c5f315038224ddb75c277ebc51ebe4c5cf24
ayomide-code <sholajaayomide10@gmail.com>
2022-09-14 17:53:31 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putcahr((prod / 10) + '0');
+				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
