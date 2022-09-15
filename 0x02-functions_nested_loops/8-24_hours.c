468c83bd58d17c5c9a3ae16540642757db4d25ce
ayomide-code <sholajaayomide10@gmail.com>
2022-09-14 17:27:28 -0700

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - function to print every minute
+ * return: 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putcahr(minutes / 10 + '0');
+			minutes++;
+			_putchar('\n');
+			hours++;
+			minutes = 0;
+		}
+	}
