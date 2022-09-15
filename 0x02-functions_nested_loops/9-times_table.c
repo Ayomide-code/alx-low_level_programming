43a13fb0a31b034ae002e99c483cd05146b760ac
ayomide-code <sholajaayomide10@gmail.com>
2022-09-14 17:51:06 -0700

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - function that prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int num1, mult, prod;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num1 * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putcahr((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
