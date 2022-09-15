150971df348ba9d68042724ace8542fbca336f9f
ayomide-code <sholajaayomide10@gmail.com>
2022-09-15 15:22:48 -0700

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void positive_or_negative(int i);
+#include <stdio.h>
+
+void postive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 int convert_day(int, int);
 
 #endif /* MAIN_H */
+
