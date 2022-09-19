24055b6ee11c45e215bb5b7b36a76bf9841a595b
ayomide-code <sholajaayomide10@gmail.com>
2022-09-19 16:33:30 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*puts2 - prints every other character
+*@str: string
+*Return: 0
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+{
+	if (i % 2 == 0)
+	putchar(*(str + i));
+i++;
+}
+putchar(10);
