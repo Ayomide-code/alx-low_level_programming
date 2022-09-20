b5deb9a1da98cb04e18ae5bffa31c465646a7675
ayomide-code <sholajaayomide10@gmail.com>
2022-09-19 18:13:20 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,28 @@
-#include <
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: input
+ * @return: half input
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+	_putchar('\n');
+}
+
