aa43ed80ad63a9f9549ba8f8b056ab024c6aafec
ayomide-code <sholajaayomide10@gmail.com>
2022-09-23 16:17:41 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,49 +3,43 @@
 
 /**
 *cap_string - a function that capitalizes all words of a string.
-*@s: An input string to capitalize letters
-*Return: pointer to s
+*@s: pointer to char input array
+*Return: @s
 */
 
 char *cap_string(char *s)
 {
-	int i = 0;
-
-	while (s[i])
-	{
-	if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-	s[i] -= 32;
-
-	if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-	s[i + 1] -= 32;
-	i++;
-}
-return (s);
-}
-
+		int i = 0;
+		/*iterate through our array values*/
+		while (s[i] != '\0')
+		{
+	/*check for any lowercase letters*/
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				/**
+				 * if we have a null character
+				 * change its value to capital
+				 */
+				if (i == 0)
+				{
+					s[i] -= 32;
+				}
 /**
- * check_seperators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?
- */
+*if we find any character matching the below before any small
+*letter we change that value to a capital letter.
+*/
 
-int check_seperators(char c)
+if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 || s[i - 1] == 44 ||
+s[i - 1] == 59 || s[i - 1] == 46 || s[i - 1] == 33 ||
+s[i - 1] == 63 || s[i - 1] == 34 || s[i - 1] == 40 ||
+s[i - 1] == 41 || s[i - 1] == 123 || s[i - 1] == 124)
 
 {
+					s[i] -= 32;
+				}
+			}
 
-	int i = 0;
-
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?','", '(', ')', '{', '}' };
-
-	for (; i < 13; i++)
-
-	{
-
-		if (c == seperators[i])
-
-			return (1);
-
-	}
-
-	return (0);
-
+			i++;
+		}
+		return (s);
 }
