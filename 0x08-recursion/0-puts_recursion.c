f2898b5cb54d0cd13fb67551ba298c634bece8fe
ayomide-code <sholajaayomide10@gmail.com>
2022-09-28 14:57:08 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/*
- * _puts_recursion - Prints a string, followed by a new line.
+/**
+ * _puts_recursion -Prints a string, followed by a new line.
  * @s: The string to be printed.
  */
 
